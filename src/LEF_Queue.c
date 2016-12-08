65936620a9e824386c8bf031ac4dfe9693cb99a6
Peter <peter.malmberg@gmail.com>
2016-12-08 16:50:39 +0100

    Functions.

---
+++ src/LEF_Queue.c
@@ -0,0 +1,37 @@
+/**
+ * LEF - Lightweight Event Framework
+ *
+ * This file is part of LEF distribution
+ *
+ *---------------------------------------------------------------------------
+ * @brief   Queue functions
+ *
+ * @file    LEF_Queue.c
+ * @author  Peter Malmberg <peter.malmberg@gmail.com>
+ * @date    2016-12-08
+ * @licence GPLv2
+ *
+ *---------------------------------------------------------------------------
+ *
+ * LEF is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  Full license text is available on the following
+ *
+ * Source repository:
+ * https://github.com/zonbrisad/LEF
+ *
+ * 1 tab = 2 spaces
+ */
+
+// Includes ---------------------------------------------------------------
+
+#include "LEF_Queue.h"
+
+// Macros -----------------------------------------------------------------
+
+// Variables --------------------------------------------------------------
+
+// Prototypes -------------------------------------------------------------
+
+// Code -------------------------------------------------------------------
+
