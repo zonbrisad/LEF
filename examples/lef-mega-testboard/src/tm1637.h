b668555241d813667bc0262fc649bcdfa10e0167
Peter Malmberg <peter.malmberg@gmail.com>
2026-03-31 14:00:07 +0200

    Doc

--- examples/lef-mega-testboard/src/tm1637.h
+++ examples/lef-mega-testboard/src/tm1637.h
@@ -6,7 +6,12 @@
  * 
  * IMORTANT!
  * The databuss used by the controller is NOT compatible with I2C and can NOT be used on the same bus with other
- * I2C units. A separate bus is needed for the TM1637.
+ * I2C units. A separate bus is needed for the TM1637
+ * 
+ * Todo:
+ * Add support for ":"
+ * Add support for ","
+ * Add support for letters
  */
 
 #pragma once
@@ -24,14 +29,21 @@ inline void tm_clk_low() { gpio_write(TM_CLK, 0); }
 inline void dm_dio_high() { gpio_write(TM_DIO, 1); }
 inline void tm_dio_low() { gpio_write(TM_DIO, 0); }
 
-
-
 inline void tm_init() {
     gpio_init(TM_CLK, GPIO_OUTPUT, GPIO_NO_PULLUP);
     gpio_init(TM_DIO, GPIO_OUTPUT, GPIO_NO_PULLUP);
 }
 
+/**
+ * Write value do display.
+ * 
+ * @param num Value to be displayed
+ */
 void tm_display_number(int num);
 
-// Command 3: display control (brightness 0–7)
+/**
+ * Set display brightness.
+ * 
+ * @param brightness 0 = off, 7 = max brightness
+ */
 void tm_display_brightness(uint8_t brightness);
